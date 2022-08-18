d8437d51eda4f5a68e335391ce237ae637920e1f
rav4353 <ravanthsri20@gmail.com>
2022-08-18 11:00:06 +0530

    hi

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// Arithmetic Operators
+// +
+
+let x = 5;
+let y = 2;
+let z = x + y;
+document.getElementById("plus").innerHTML = z;
+
+// -
+
+let a = 5;
+let b = 2;
+let c = a - b;
+document.getElementById("minus").innerHTML = c;
+
+// *
+
+let d = 5;
+let e = 2;
+let f = d * e;
+document.getElementById("multiply").innerHTML = f;
+
+// /
+
+let g = 5;
+let h = 2;
+let i = g / h;
+document.getElementById("divide").innerHTML = i;
+
+// Exponentiation **
+
+let j = 5;
+let k = 2;
+let l = j ** k;
+document.getElementById("Exponentiation").innerHTML = l;
+
+// 	Modulus %
+
+let m = 5;
+let n = 2;
+let o = m % n;
+document.getElementById("Modulus").innerHTML = o;
+
+// Increment  ++
+
+let p = 5;
+let q = ++p;
+document.getElementById("Increment").innerHTML = q;
+
+// Decrement --
+
+let r = 5;
+let s = --r;
+document.getElementById("Decrement").innerHTML = s;
+
+//Assignment Operators
+
+// =	x = y	x = y
+
+var aa = 10;
+aa = 5;
+document.getElementById("equalto").innerHTML = aa;
+
+// +=	x += y	x = x + y
+
+var bb = 10;
+bb += 5;
+document.getElementById("plusequalto").innerHTML = bb;
+
+// -=	x -= y	x = x - y
+
+
+
+// *=	x *= y	x = x * y
+
+
+
+// /=	x /= y	x = x / y
+
+
+
+// %=	x %= y	x = x % y
+
+
+
+// **=	x **= y	x = x ** y

99cedf1ba6b1b27d2adc2f26920118da61e5013c
rav4353 <ravanthsri20@gmail.com>
2022-08-18 14:20:02 +0530

    hi

--- js/script.js
+++ js/script.js
@@ -69,18 +69,157 @@ document.getElementById("plusequalto").innerHTML = bb;
 
 // -=	x -= y	x = x - y
 
+var cc = 10;
+cc -= 5;
+document.getElementById("minusequalto").innerHTML = cc;
 
 
 // *=	x *= y	x = x * y
 
+var dd = 10;
+dd *= 5;
+document.getElementById("multiplyequalto").innerHTML = dd;
 
 
 // /=	x /= y	x = x / y
 
+var ee = 10;
+ee /= 5;
+document.getElementById("divideequalto").innerHTML = ee;
 
 
 // %=	x %= y	x = x % y
 
+var ff = 10;
+ff %= 5;
+document.getElementById("modulesequalto").innerHTML = ff;
 
 
 // **=	x **= y	x = x ** y
+
+var gg = 10;
+gg **= 5;
+document.getElementById("multiplymultiplyequalto").innerHTML = gg;
+
+// String Operators
+
+// Adding two string
+
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+document.getElementById("string").innerHTML = text3;
+
+// Adding string and number
+
+let sn = "Hello" + 5;
+document.getElementById("stringandnumber").innerHTML = sn;
+
+// Comparison Operators
+
+// ==	equal to
+
+let hh = 5;
+document.getElementById("comparisona").innerHTML = (hh == 8);
+
+// ===	equal value and equal type
+
+let ii = 5;
+document.getElementById("comparisonb").innerHTML = (ii === 5);
+
+// !=	not equal
+
+let jj = 5;
+document.getElementById("comparisonc").innerHTML = (jj != 8);
+
+// !==	not equal value or not equal type
+
+let kk = 5;
+document.getElementById("comparisond").innerHTML = (kk !== 8);
+
+// >	greater than
+
+let ll = 5;
+document.getElementById("comparisone").innerHTML = (ll > 8);
+
+// <	less than
+
+let mm = 5;
+document.getElementById("comparisonf").innerHTML = (mm < 8);
+
+// >=	greater than or equal to
+
+let nn = 5;
+document.getElementById("comparisong").innerHTML = (nn >= 8);
+
+// <=	less than or equal to
+
+let oo = 5;
+document.getElementById("comparisonh").innerHTML = (oo <= 8);
+
+// Logical Operators
+
+// &&	and
+
+let pp = 6;
+let qq = 3;
+
+document.getElementById("demo1").innerHTML = 
+(pp < 10 && qq > 1) + "<br>" + 
+(pp < 10 && qq < 1);
+
+// ||	or
+
+let rr = 6;
+let ss = 3;
+
+document.getElementById("demo2").innerHTML = 
+(rr == 5 || ss== 5) + "<br>" + 
+(rr == 6 || ss== 0) + "<br>" + 
+(rr == 0 || ss== 3) + "<br>" + 
+(rr == 6 || ss== 3);
+
+// !	not
+
+let tt = 6;
+let uu = 3;
+
+document.getElementById("demo3").innerHTML = 
+!(tt === uu) + "<br>" + 
+!(tt > uu);
+
+// Type Operators
+
+let aaa = 5;
+document.getElementById("type").innerHTML = typeof aaa;
+
+
+// Bitwise Operators
+
+// AND
+
+document.getElementById("and").innerHTML = 5 & 1;
+
+// OR 
+
+document.getElementById("or").innerHTML = 5 | 1;
+
+// XOR
+
+document.getElementById("xor").innerHTML = 5 ^ 1;
+
+// NOT
+
+document.getElementById("not").innerHTML = ~ 5;
+
+// LEFT SHIFT
+
+document.getElementById("lshift").innerHTML = 5 << 1;
+
+// RIGHT SHIFT
+
+document.getElementById("rshift").innerHTML = -5 >> 1;
+
+// UNSIGNED RIHGT SHIFT 
+
+document.getElementById("urshift").innerHTML = 5 >>> 1;
